e517e99023bc3409a406586c17cdd903fba78a58
Jazeb_Javed <jazebjaved52@gmail.com>
2023-12-12 01:22:39 +0500

    get api done

--- provider_project_1/lib/Providers/apiProvider.dart
+++ provider_project_1/lib/Providers/apiProvider.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:provider_project_1/Views/apiViews/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider_project_1/models/postModel.dart';
 
-class APIProvider extends ChangeNotifier{
-    
+class APIProvider extends ChangeNotifier {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPosts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if (response.statusCode == 200) {
+      // var data = response.body;
+      var data = jsonDecode(response.body);
+      // print("${data}");
+      for (var i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+    } else {
+      print("api failed");
+    }
+    return postList;
+  }
 }
--- provider_project_1/lib/Views/apiViews/get.dart
+++ provider_project_1/lib/Views/apiViews/get.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_project_1/Providers/apiProvider.dart';
 
 class GetApi extends StatelessWidget {
   const GetApi({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final apiProvider = Provider.of<APIProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Get API'),
+        title: const Text('API Get'),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Get API'),
-          ],
-        ),
+      body: FutureBuilder(
+        future: apiProvider.getPosts(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text("Loading"),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: apiProvider.postList.length,
+              itemBuilder: (context, index) {
+                return Text("${apiProvider.postList[index].title}");
+              },
+            );
+          }
+        },
       ),
     );
   }
 }
+
+// FutureBuilder(
+//           future: postList,
+//           builder: (context, snapshot) {
+//             if (!snapshot.hasData) {
+//               return Text("Loading");
+//             } else {
+//               return ListView.builder(
+//                 itemCount: apiProvider.postList.length,
+//                 itemBuilder: (context, index) {
+//                   return Text("${apiProvider.postList[index].title}");
+//                 },
+//               );
+//             }
+//           },
+//         ),
